957d11abff4459bddc0d80baab26b888ab078852
BrianHenriksen <brian.henriksen@gokstadakademiet.no>
2024-01-20 16:35:33 +0100

    Added comment

--- Git Hello World/Program.cs
+++ Git Hello World/Program.cs
@@ -17,3 +17,7 @@ class Program
         }
     }
 }
+
+// A question for Craig, should you see it.
+// while testing git for a bit, the main branch was always named MAIN.
+// but in this case it shows as MASTER.. and i dont know why.
